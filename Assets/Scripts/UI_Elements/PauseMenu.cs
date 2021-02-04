65b45c04c577c34190a1a79d5b9584171b70cd8d
Anthony Beaudoin <abeaudoin2@hotmail.ca>
2021-02-04 11:25:28 -0500

    Added basic Hud with pause option

---
+++ Assets/Scripts/UI_Elements/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI_Elements
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public GameObject pauseMenu;
+        public GameObject validationMenu;
+    
+        private bool _isPaused = false;
+
+        public void Update()
+        {
+            //TODO there is a better way to map this probably
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _isPaused = !_isPaused;
+                if (validationMenu.activeSelf)
+                {
+                    validationMenu.SetActive(false);
+                }
+            }
+
+            if (_isPaused)
+            {
+                TriggerPauseOn();
+            }
+            else
+            {
+                TriggerPauseOff();
+            }
+        }
+
+        public void TriggerPauseOn()
+        {
+            pauseMenu.SetActive(true);
+            _isPaused = true;
+        }
+    
+        public void TriggerPauseOff()
+        {
+            pauseMenu.SetActive(false);
+            _isPaused = false;
+        }
+
+        public void LogOut()
+        {
+            Debug.Log("Logging out");
+            // SceneManager.LoadScene(0); //Laoding back the menu scene
+        }
+    
+    }
+}
