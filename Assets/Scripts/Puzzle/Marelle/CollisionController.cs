a6ae492ebeb9aca0e4ffc079e85149292134968e
Élie Bédard <elbed16@ulaval.ca>
2021-03-08 14:53:56 -0500

    name change

--- Assets/Scripts/Puzzle/Marelle/CollisionController.cs
+++ Assets/Scripts/Puzzle/Marelle/CollisionController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 
-public class PuzzleMechanics : MonoBehaviour
+public class CollisionController : MonoBehaviour
 {
     [SerializeField] private float raycastRange = 0.1f;
     [SerializeField] private GameObject bottomObject;
