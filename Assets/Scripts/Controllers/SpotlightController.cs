804ac51bfeef59e1cd18b84a275e6aeb896f15e3
Anthony Beaudoin <abeaudoin2@hotmail.ca>
2021-03-09 11:03:43 -0500

    Cleanup in scene + fixed spotlight control + moved press. plate to door

--- Assets/Scripts/Controllers/SpotlightController.cs
+++ Assets/Scripts/Controllers/SpotlightController.cs
@@ -15,15 +15,15 @@ public class SpotlightController : CustomController
     
     public override void MoveAtMaxSpeed(float verticalMotion, float horizontalMotion, float timeElapsed)
     {
-        controller.Move(- transform.forward * (verticalMotion * timeElapsed * movementSpeed));
-        controller.Move(- transform.right * (horizontalMotion * timeElapsed * movementSpeed));
+        controller.Move(transform.forward * (verticalMotion * timeElapsed * movementSpeed));
+        controller.Move(transform.right * (horizontalMotion * timeElapsed * movementSpeed));
     }
     
 
     public override void Move(Vector3 speed, float timeElapsed)
     {
-        controller.Move(- transform.forward * (speed.z * timeElapsed));
-        controller.Move(- transform.right * (speed.x * timeElapsed));
+        controller.Move( transform.forward * (speed.z * timeElapsed));
+        controller.Move( transform.right * (speed.x * timeElapsed));
     }
 
 }
