392325a664b2813f2aea4d7cf0f4bf93990270be
Élie Bédard <elbed16@ulaval.ca>
2021-03-31 22:53:01 -0400

    pu de fe rpc

--- Assets/Scripts/Puzzle/Marelle/MarelleController.cs
+++ Assets/Scripts/Puzzle/Marelle/MarelleController.cs
@@ -35,59 +35,34 @@ public class MarelleController : MonoBehaviour
     }
     public void gameWon()
     {
-        //audioSource.PlayOneShot(winSound, 0.7f);
-        //hasCollisionUnlocked = false;
-        //foreach (Actionable a in actionableObject)
-        //{
-        //    a.OnAction();
-        //}
-
-        _photonView.RPC("rcpGameWon", RpcTarget.All);
-
-    }
-    [PunRPC]
-    private void rcpGameWon()
-    {
         audioSource.PlayOneShot(winSound, 0.7f);
         hasCollisionUnlocked = false;
         foreach (Actionable a in actionableObject)
         {
-            if (!a.hasActioned)
-            {
-                a.OnAction();
-            }
+            a.OnAction();
         }
+
+        // _photonView.RPC("rcpGameWon", RpcTarget.All);
+
+    }
+    //[PunRPC]
+    //private void rcpGameWon()
+    //{
+    //    audioSource.PlayOneShot(winSound, 0.7f);
+    //    hasCollisionUnlocked = false;
+    //    foreach (Actionable a in actionableObject)
+    //    {
+    //        if (!a.hasActioned)
+    //        {
+    //            a.OnAction();
+    //        }
+    //    }
             
 
-    }
+    //}
     public void gameLost()
     {
-        _photonView.RPC("rcpGameLost", RpcTarget.All);
-        //audioSource.PlayOneShot(lossSound, 0.7f);
-        //if (UnityEngine.Random.Range(0, 2) == 0)//50%
-        //{
-        //    robotFail.PlayTaskFailed();
-        //}
-        //hasCollisionUnlocked = false;
-        //foreach (Transform child in transform)
-        //{
-
-        //    foreach (Transform toddler in child)
-        //    {
-
-        //        Material tileMat = toddler.GetComponentInChildren<TileGoUpDown>().tileRenderer.material;
-
-        //        tileMat.SetColor("_Color", Color.red);
-
-        //        toddler.GetComponent<TileGoUpDown>().PlayAnimation();
-
-        //    }
-        //}
-    }
-
-    [PunRPC]
-    public void rcpGameLost()
-    {
+        // _photonView.RPC("rcpGameLost", RpcTarget.All);
         audioSource.PlayOneShot(lossSound, 0.7f);
         if (UnityEngine.Random.Range(0, 2) == 0)//50%
         {
@@ -109,4 +84,29 @@ public class MarelleController : MonoBehaviour
             }
         }
     }
+
+    //[PunRPC]
+    //public void rcpGameLost()
+    //{
+    //    audioSource.PlayOneShot(lossSound, 0.7f);
+    //    if (UnityEngine.Random.Range(0, 2) == 0)//50%
+    //    {
+    //        robotFail.PlayTaskFailed();
+    //    }
+    //    hasCollisionUnlocked = false;
+    //    foreach (Transform child in transform)
+    //    {
+
+    //        foreach (Transform toddler in child)
+    //        {
+
+    //            Material tileMat = toddler.GetComponentInChildren<TileGoUpDown>().tileRenderer.material;
+
+    //            tileMat.SetColor("_Color", Color.red);
+
+    //            toddler.GetComponent<TileGoUpDown>().PlayAnimation();
+
+    //        }
+    //    }
+    //}
 }
