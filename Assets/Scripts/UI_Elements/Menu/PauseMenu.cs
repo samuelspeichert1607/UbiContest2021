d675b435ccb37a8959b1d9bcc903bea98d2c8a60
Anthony Beaudoin <abeaudoin2@hotmail.ca>
2021-04-06 10:30:38 -0400

    Fixed bug when exiting menu with controls opened
    When opening menu again, controls where visible

--- Assets/Scripts/UI_Elements/Menu/PauseMenu.cs
+++ Assets/Scripts/UI_Elements/Menu/PauseMenu.cs
@@ -46,10 +46,6 @@ namespace UI_Elements
                     && !playerController.IsInCriticalMotion())
                 {
                     pauseUnPause();
-                    if (validationMenu.activeSelf)
-                    {
-                        validationMenu.SetActive(false);
-                    }
                 }
                 if (HasNavigatedInMenu())
                 {
@@ -101,6 +97,8 @@ namespace UI_Elements
         private void UnPause()
         {
             pauseMenu.SetActive(false);
+            validationMenu.SetActive(false);
+            controlsMenu.SetActive(false);
             _audioPlayer.PlayBackSound();
             Invoke(nameof(AllowPlayerMovement), 0.05f);
         }
