3628bd697b2636652ab6839c12136de8d7e32c84
Élie Bédard <elbed16@ulaval.ca>
2021-04-06 22:04:02 -0400

    salle synchro blocking

--- Assets/Scripts/UI_Elements/Menu/EndScreen.cs
+++ Assets/Scripts/UI_Elements/Menu/EndScreen.cs
@@ -9,21 +9,26 @@ public class EndScreen : MonoBehaviour
     [SerializeField] private float loadDelay;
     [SerializeField] private FadableText textToAppear;
     [SerializeField] private PulsateText pulsateText;
-    
+    private bool hasStartedPulsating = false;
+
     void Start()
     {
         textToAppear.StartCoroutine("Start");
         textToAppear.SetAlphaToZero();
         textToAppear.FadeToFullAlpha();
-        pulsateText.enabled = false;
+        pulsateText.StartCoroutine("Start");
+        pulsateText.SetAlphaToZero();
     }
 
     void Update()
     {
         if (textToAppear.IsTransitionDone())
         {
-            pulsateText.enabled = true;
-            pulsateText.StartPulsating();
+            if (!hasStartedPulsating)
+            {
+                pulsateText.StartPulsating();
+                hasStartedPulsating = true;
+            }
             if (Input.anyKeyDown)
             {
                 Invoke(nameof(LoadMenu), loadDelay);
