6f2e7dd79371479b9c0c8be66cc8a3bc0ae5863a
Élie Bédard <elbed16@ulaval.ca>
2021-04-07 17:54:12 -0400

    decorations

--- Assets/Scripts/UI_Elements/Hud/StatusHUD.cs
+++ Assets/Scripts/UI_Elements/Hud/StatusHUD.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using TMPro;
-using TreeEditor;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
